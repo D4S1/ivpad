eb41b78e3efc7a70640f443d196dae727fdb6544
Asia <joanna.bastet@gmail.com>
2025-04-06 22:57:01 +0200

    create docs and format

--- page.py
+++ page.py
@@ -1,23 +1,46 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template
 import utils
-
 
 app = Flask(__name__)
 
-# Wczytanie danych – przy uruchomieniu aplikacji
 excel_file = "data/NIHMS1635539-supplement-1635539_Sup_tab_4.xlsx"
 df_box = utils.preprocess_values(excel_file)
 
 
 @app.route("/")
 def index():
+    """
+    Renders the index page with the volcano plot and box plot for a given gene.
+
+    This route loads the volcano plot and the box plot for the gene 'STUB1' when the user visits the home page.
+    It then renders the 'main.html' template with the respective plots.
+
+    Returns:
+    str: The HTML page generated from the 'main.html' template with embedded plots.
+    """
     vulcano_plot, div_id = utils.plot_vulacano(excel_file)
     box_plot = utils.plot_gene(df_box, 'STUB1')
-    return render_template("main.html", vulcano_plot=vulcano_plot, vulcano_id=div_id, box_plot=box_plot)
+    return render_template(
+        "main.html",
+        vulcano_plot=vulcano_plot,
+        vulcano_id=div_id,
+        box_plot=box_plot
+    )
 
 
 @app.route("/boxplot/<gene>")
 def show_boxplot(gene):
+    """
+    Renders a box plot for a specified gene.
+
+    This route allows the user to view a box plot for any given gene by passing the gene's name in the URL.
+    
+    Parameters:
+    gene (str): The gene for which the box plot will be displayed.
+
+    Returns:
+    str: The HTML div of the box plot for the specified gene.
+    """
     return utils.plot_gene(df_box, gene)
 
 
--- utils.py
+++ utils.py
@@ -5,17 +5,36 @@ from plotly.offline import plot
 import re
 
 
-def preprocess_values(excel_file):
+def preprocess_values(excel_file: str) -> pd.DataFrame:
+    """
+    Preprocess values from an Excel file containing gene expression data.
 
+    Parameters:
+    excel_file (str): The path to the Excel file containing the data.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing the 'EntrezGeneID', 'EntrezGeneSymbol', 'Organism', 
+                  and relevant sample columns that include 'YD' or 'OD' in the sample names.
+    """
     df_box = pd.read_excel(excel_file, sheet_name="S4A values", header=2, index_col=0)
 
     relevant_idx = list(df_box.columns).index('Set002.H4.OD12.dup')
-    samples = [sample for sample in list(df_box.columns)[relevant_idx:] if 'YD' in sample or 'OD' in sample]
+    samples = [sample for sample in list(df_box.columns)[relevant_idx:]
+               if 'YD' in sample or 'OD' in sample]
 
     return df_box[["EntrezGeneID", "EntrezGeneSymbol", "Organism"] + samples]
 
 
 def plot_vulacano(file: str):
+    """
+    Generate a volcano plot for differential protein expression from a given Excel file.
+
+    Parameters:
+    file (str): The path to the Excel file containing the data.
+
+    Returns:
+    tuple: The HTML div of the plot and the div ID.
+    """
     df_volcano = pd.read_excel(file, sheet_name="S4B limma results", header=2, index_col=0)
     df_volcano['neglogP'] = -np.log10(df_volcano['adj.P.Val'])
 
@@ -28,20 +47,42 @@ def plot_vulacano(file: str):
         title='Volcano Plot of Differential Protein Expression',
     )
 
+    fig.update_traces(
+        marker=dict(size=10, opacity=0.6)
+    )
+
+    fig.update_layout(
+        title_font_size=20,
+        xaxis_title_font_size=15,
+        yaxis_title_font_size=15,
+        font=dict(size=14),
+        height=600
+    )
+
     fig.update_traces(textposition='top center')
-    
+
     vulcano = plot(
         fig,
         output_type='div',
         include_plotlyjs=True,
         config={'displayModeBar': False},
-        )
+    )
     div_id = re.search('<div id="([^"]*)"', vulcano).groups()[0]
 
     return vulcano, div_id
 
 
 def plot_gene(data: pd.DataFrame, gene: str):
+    """
+    Generate a boxplot comparing protein concentrations between young and old age groups for a given gene.
+
+    Parameters:
+    data (pd.DataFrame): The dataframe containing protein concentration data.
+    gene (str): The gene to plot.
+
+    Returns:
+    str: The HTML div of the plot.
+    """
     data = data[data.EntrezGeneSymbol == gene].T
     data = data.iloc[3:]
 
@@ -49,25 +90,37 @@ def plot_gene(data: pd.DataFrame, gene: str):
     data["age_group"] = data.index.map(lambda x: "Old" if "OD" in x else "Young")
 
     fig = px.box(
-                data,
-                x="age_group",
-                y="value",
-                points="all",
-                title=f"Protein Concentration: Young vs. Old for {gene}",
-                labels={
-                    "age_group": "Donor Age Group",
-                    "value": "Protein Concentration"
-                },
-                )
+        data,
+        x="age_group",
+        y="value",
+        points="all",
+        color="age_group",
+        title=f"Protein Concentration: Young vs. Old for {gene}",
+        labels={
+            "age_group": "Donor Age Group",
+            "value": "Protein Concentration"
+        },
+    )
+
     fig.update_traces(
         jitter=0.3,
         pointpos=0,
         hoverinfo='skip',
-        marker=dict(opacity=0.6)
+        marker=dict(opacity=0.6),
+        showlegend=False
     )
+
+    fig.update_layout(
+        title_font_size=20,
+        xaxis_title_font_size=15,
+        yaxis_title_font_size=15,
+        font=dict(size=14),
+        height=600
+    )
+
     return plot(
         fig,
         output_type='div',
         include_plotlyjs=False,
         config={'displayModeBar': False},
-        )
+    )
