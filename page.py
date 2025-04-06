8f450859610026d80b9c60799ead3deeae94ce76
Asia <joanna.bastet@gmail.com>
2025-04-06 18:19:22 +0200

    utils + boxplot

--- page.py
+++ page.py
@@ -1,40 +1,18 @@
 from flask import Flask, render_template, jsonify, request
-import pandas as pd
-import numpy as np
-import plotly.express as px
-from plotly.offline import plot
-import plotly
-import json
+import utils
+
 
 app = Flask(__name__)
 
 # Wczytanie danych – przy uruchomieniu aplikacji
 excel_file = "data/NIHMS1635539-supplement-1635539_Sup_tab_4.xlsx"
+df_box = utils.preprocess_values(excel_file)
 
-
-
-df_box = pd.read_excel(excel_file, sheet_name="S4A values", header=2, index_col=0)
-
-def plot_vulacano(file):
-    df_volcano = pd.read_excel(file, sheet_name="S4B limma results", header=2, index_col=0)
-    df_volcano['neglogP'] = -np.log10(df_volcano['adj.P.Val'])
-
-    fig = px.scatter(
-        df_volcano,
-        x='logFC',
-        y='neglogP',
-        hover_name='TargetFullName',
-        labels={'logFC': 'log2 Fold Change', 'neglogP': '-log10 Adjusted P-Value'},
-        title='Volcano Plot of Differential Protein Expression',
-    )
-
-    fig.update_traces(textposition='top center')
-    
-    return plot(fig, output_type='div', include_plotlyjs=False)
+utils.plot_gene(df_box, 'STUB1')
 
 @app.route("/")
 def index():
-    vulcano_plot = plot_vulacano(excel_file)
+    vulcano_plot = utils.plot_vulacano(excel_file)
     return render_template("main.html", vulcano_plot=vulcano_plot)
 
 
---
+++ utils.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly.offline import plot
+
+
+def preprocess_values(excel_file):
+
+    df_box = pd.read_excel(excel_file, sheet_name="S4A values", header=2, index_col=0)
+
+    relevant_idx = list(df_box.columns).index('Set002.H4.OD12.dup')
+    samples = [sample for sample in list(df_box.columns)[relevant_idx:] if 'YD' in sample or 'OD' in sample]
+
+    return df_box[["EntrezGeneID", "EntrezGeneSymbol", "Organism"] + samples]
+
+
+def plot_vulacano(file: str):
+    df_volcano = pd.read_excel(file, sheet_name="S4B limma results", header=2, index_col=0)
+    df_volcano['neglogP'] = -np.log10(df_volcano['adj.P.Val'])
+
+    fig = px.scatter(
+        df_volcano,
+        x='logFC',
+        y='neglogP',
+        hover_name='EntrezGeneSymbol',
+        labels={'logFC': 'log2 Fold Change', 'neglogP': '-log10 Adjusted P-Value'},
+        title='Volcano Plot of Differential Protein Expression',
+    )
+
+    fig.update_traces(textposition='top center')
+    
+    return plot(fig,
+                output_type='div',
+                include_plotlyjs=False,
+                config={'displayModeBar': False},
+                )
+
+
+def plot_gene(data: pd.DataFrame, gene: str):
+    data = data[data.EntrezGeneSymbol == gene].T
+    data = data.iloc[3:]
+
+    data["value"] = data.iloc[:, 0].astype(float)
+    data["age_group"] = data.index.map(lambda x: "Old" if "OD" in x else "Young")
+
+    fig = px.box(data, x="age_group", y="value", points="all", title=f"Boxplot for {gene} gene")
+    fig.update_layout(height=400)
+    fig.update_traces(
+        jitter=0.3,
+        pointpos=0
+    )
+    fig.show()
+
+    # boxplot_div = plot(fig, output_type="div", include_plotlyjs=False)
+    # return boxplot_div
